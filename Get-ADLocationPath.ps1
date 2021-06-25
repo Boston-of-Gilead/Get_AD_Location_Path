72d6a80ddc6ce853f8ed5e2bafde8705dcac7ee6
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-25 10:28:36 -0400

    Add files via upload

---
+++ Get-ADLocationPath.ps1
@@ -0,0 +1,14 @@
+﻿#Get the OU of a worker, and their manager
+
+function Get-ADLocationPath {
+    param (
+        $FindThisGuy
+    )
+    $UserObj = Get-ADUser $FindThisGuy -Properties CN
+    $CN = $UserObj.CN
+    $DN = $UserObj.DistinguishedName
+    $DN.Replace("CN=$CN,","")
+}
+
+$worker = Get-ADLocationPath <user>
+$bosspath = Get-ADLocationPath $Manager
